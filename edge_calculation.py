8192e89454c346952f6a5e3408338a7552afc518
shanzhaowang99 <96216127+shanzhaowang99@users.noreply.github.com>
2022-04-13 18:23:15 -0400

    Add files via upload

---
+++ edge_calculation.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+import os
+
+
+class MonoOdometery(object):
+    def __init__(self,
+                img_file_path,
+                focal_length = 718.8560,
+                pp = (607.1928, 185.2157), 
+                lk_params=dict(winSize  = (21,21), criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01)),
+                detector=cv.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)):
+        '''
+        Arguments:
+            img_file_path {str} -- File path that leads to image sequences
+        
+        Keyword Arguments:
+            focal_length {float} -- Focal length of camera used in image sequence (default: {718.8560})
+            pp {tuple} -- Principal point of camera in image sequence (default: {(607.1928, 185.2157)})
+            lk_params {dict} -- Parameters for Lucas Kanade optical flow
+            detector {cv2.FeatureDetector} -- Most types of OpenCV feature detectors (default: {cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)})
+
+        '''
+
+        self.file_path = img_file_path
+        self.detector = detector
+        self.lk_params = lk_params
+        self.focal = focal_length
+        self.pp = pp
+        self.R = np.zeros(shape=(3, 3))
+        self.t = np.zeros(shape=(3, 3))
+
+
+    def detect(self, img):
+        '''Used to detect features and parse into useable format
+
+        
+        Arguments:
+            img {np.ndarray} -- Image for which to detect keypoints on
+        
+        Returns:
+            np.array -- A sequence of points in (x, y) coordinate format
+            denoting location of detected keypoint
+        '''
+
+        p0 = self.detector.detect(img)
+        
+        return np.array([x.pt for x in p0], dtype=np.float32).reshape(-1, 1, 2)
+
+
+    def visual_odometery(self):
+        '''
+        Used to perform visual odometery.
+
+        Returns:
+            R -- Rotation matrix
+            denoting location of detected keypoint
+        '''
+
+        self.p0 = self.detect(self.old_frame)
+
+
+        # Calculate optical flow between frames
+        self.p1, st, err = cv.calcOpticalFlowPyrLK(self.old_frame, self.current_frame, self.p0, None, **self.lk_params)
+        
+
+        # Save the good points from the optical flow
+        self.good_old = self.p0[st == 1]
+        self.good_new = self.p1[st == 1]
+
+
+        # Calculate the rotation matrix R and translation t
+        E, _ = cv.findEssentialMat(self.good_new, self.good_old, self.focal, self.pp, cv.RANSAC, 0.999, 1.0, None)
+        _, R, t, _ = cv.recoverPose(E, self.good_old, self.good_new, focal=self.focal, pp=self.pp)
+        return R, t
+
+
+    def process_frame(self, id0, id1):
+        '''
+        Get pair images
+
+        Arguments:
+            id0 -- id of the first pair image
+            id1 -- id of the second pair image
+
+        '''
+
+        self.old_frame = cv.imread(self.file_path + str(id0) + '.jpg', 0)
+        self.current_frame = cv.imread(self.file_path + str(id1) + '.jpg', 0)
+
+    def img_to_frame(self, img0, img1):
+       '''
+       Directly load images to frame
+
+       Arguments:
+            img0 -- first pair image
+            img1 -- second pair image
+
+       '''
+       self.old_frame = cv.cvtColor(img0, cv.COLOR_BGR2GRAY)
+       self.current_frame = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
---
+++ utils.py
@@ -0,0 +1,84 @@
+import math
+import numpy as np
+
+
+def eul2rotm(theta) :
+
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
+                    ])
+
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
+                    ])
+
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+
+    return R
+
+def eul2rotm_y_x_z(theta) :
+
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[1]), -math.sin(theta[1]) ],
+                    [0,         math.sin(theta[1]), math.cos(theta[1])  ]
+                    ])
+
+    R_y = np.array([[math.cos(theta[0]),    0,      math.sin(theta[0])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[0]),   0,      math.cos(theta[0])  ]
+                    ])
+
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+
+    R = np.dot(R_y, np.dot( R_x, R_z ))
+
+    return R
+
+# Get 4x4 transformation matrix
+def rp_to_transformation(rotm, P):
+    R = np.vstack((rotm[0:3], rotm[3:6], rotm[6:9]))
+    P = np.reshape(P, (3,1))
+    T = np.vstack((np.hstack((R, P)), np.array([0, 0, 0, 1])))
+    return T
+
+
+# data in format (theta y, theta x, theta z, x, y, z)
+# vertex in format (idx, rotation matrix, x, y, z)
+def data_to_vertex(data):
+    vertex = np.zeros((data.shape[0], 13))
+    for i in range(vertex.shape[0]):
+        vertex[i,0] = i;
+        R = eul2rotm_y_x_z(data[i, 0:3])
+        vertex[i, 1:4] = R[0]
+        vertex[i, 4:7] = R[1]
+        vertex[i, 7:10] = R[2]
+        vertex[i, 10:13] = data[i, 3:6]
+
+    return vertex
+
+# edge in format (idx0, idx1, rotation matrix, x, y, z)
+def vertex_to_edge(vertex):
+    edge = np.zeros((vertex.shape[0] - 1, 14))
+    for i in range(edge.shape[0]):
+        pose_0 = rp_to_transformation(vertex[i, 1:10], vertex[i, 10:13])
+        pose_1 = rp_to_transformation(vertex[i+1, 1:10], vertex[i+1, 10:13])
+        T = np.dot(np.linalg.inv(pose_0), pose_1)
+        edge[i, 0] = i
+        edge[i, 1] = i+1
+        edge[i, 2:11] = np.hstack((T[0, :3], T[1, :3], T[2, :3]))
+        edge[i, 11:14] = T[0:3, 3]
+
+    return edge
+
+
+
